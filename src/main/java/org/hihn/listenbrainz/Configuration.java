cf2bb28c353a19f6b5b8aadc1413a45a85510e80
Rainer Hihn <hihn.rainer@gmail.com>
2024-09-27 21:16:38 +0200

    OpenAPI Code Generation

--- src/main/java/org/hihn/listenbrainz/Configuration.java
+++ src/main/java/org/hihn/listenbrainz/Configuration.java
@@ -15,7 +15,7 @@ package org.hihn.listenbrainz;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.8.0")
 public class Configuration {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
